bbf1d700dd072fcd888bdf0bb736dcb4badeb509
ixaxaar <root@ixaxaar.in>
2025-10-30 23:40:09 +0530

    add docs

--- firmware/src/bluetooth.h
+++ firmware/src/bluetooth.h
@@ -9,17 +9,29 @@
 #define SERVICE_UUID        "12345678-1234-1234-1234-123456789abc"
 #define CHARACTERISTIC_UUID "87654321-4321-4321-4321-cba987654321"
 
+/**
+ * @brief Structure to hold sensor data for BLE transmission.
+ */
 struct SensorPacket {
-    float accel_x, accel_y, accel_z;
-    float gyro_x, gyro_y, gyro_z;
-    uint8_t button_state;  // 0=none, 1=short_press, 2=long_press
-    uint32_t timestamp;
+    float accel_x, accel_y, accel_z; ///< X, Y, and Z-axis acceleration data.
+    float gyro_x, gyro_y, gyro_z;   ///< X, Y, and Z-axis gyroscope data.
+    uint8_t button_state;  // 0=none, 1=short_press, 2=long_press ///< State of the button (0=none, 1=short press, 2=long press).
+    uint32_t timestamp;             ///< Timestamp of the sensor reading.
 } __attribute__((packed));
 
-extern BLECharacteristic* pCharacteristic;
-extern bool deviceConnected;
+extern BLECharacteristic* pCharacteristic; ///< Pointer to the BLE characteristic used for sending data.
+extern bool deviceConnected;               ///< Flag to indicate if a BLE client is connected.
 
+/**
+ * @brief Initializes the Bluetooth Low Energy (BLE) server, service, and characteristic.
+ */
 void initBluetooth();
+
+/**
+ * @brief Packages and sends sensor data over BLE.
+ *
+ * @param buttonState The current state of the button to be included in the packet.
+ */
 void sendSensorData(uint8_t buttonState);
 
 #endif
--- firmware/src/sensor.h
+++ firmware/src/sensor.h
@@ -1,7 +1,22 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+/**
+ * @brief Initializes the sensor.
+ *
+ */
 void initSensor();
+
+/**
+ * @brief Gets the sensor data from the accelerometer and gyroscope.
+ *
+ * @param accel_x Pointer to a float to store the X-axis acceleration.
+ * @param accel_y Pointer to a float to store the Y-axis acceleration.
+ * @param accel_z Pointer to a float to store the Z-axis acceleration.
+ * @param gyro_x Pointer to a float to store the X-axis gyroscope data.
+ * @param gyro_y Pointer to a float to store the Y-axis gyroscope data.
+ * @param gyro_z Pointer to a float to store the Z-axis gyroscope data.
+ */
 void getSensorData(float* accel_x, float* accel_y, float* accel_z,
                    float* gyro_x, float* gyro_y, float* gyro_z);
 
